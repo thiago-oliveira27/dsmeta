5d553618520cf1617a3a7355560dd11107403d7b
Thiago Oliveira <thiagoolcarv27@gmail.com>
2022-09-05 21:39:24 -0300

    Project Created

---
+++ backend/src/test/java/com/thiago/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
