5b1d74fc515121729dd76079a7968c85a62e030c
Thiago Oliveira <thiagoolcarv27@gmail.com>
2022-09-05 23:59:08 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./assets/components/NotificationButton"
 
 function App() {
   return (
-  <h1>Olá mundo</h1>
-  )
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton />
+    </>
+    )
+    
 }
 
 export default App
